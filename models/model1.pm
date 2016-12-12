c254619b911f72fae285ce787d0d3544a2d86c2f
saifalam <aust.saif@gmail.com>
2016-12-12 21:35:56 +0100

    simple checker model and property for probabilistic durability for single disk

---
+++ models/model1.pm
@@ -0,0 +1,14 @@
+// Model1 one disk without repairs
+// Annual Failure rate 5%
+// T=1 ( year)
+//====================== Result ============================
+// result: 95.12294272563595
+
+ctmc //current time Markov chain
+
+const double fr = 0.05; // Annual Failure rate 5%
+
+module disk
+         ss: [0..1] init 1; // durable 1 , 0 failed
+	[fail] (ss=1) -> fr : (ss'=0);
+endmodule
